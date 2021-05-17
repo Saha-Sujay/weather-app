7c7b59e087b5eb44f1d08006ef1d816d0d2dd355
Saha-Sujay <gogol.sujay700@gmail.com>
2021-05-17 11:51:11 +0530

    first commit

---
+++ src/components/Titles.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+function Titles(props) {
+    return(
+        <header role="banner" className="titles-Wrapper">
+            
+            <h1>WEATHER APP</h1>
+        </header>
+    );
+}
+
+export default Titles;
